a5a436707ff1dbeb7b07fee17e9ffb22fbf4b914
bezhelev <43707287+bezhelev@users.noreply.github.com>
2018-11-08 15:06:43 +0300

    initial commit

---
+++ web/src/main/resources/static/src/components/sign-up/sign-up-service.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sign-up.css';
+import MaskedInput from 'react-text-mask';
+
+const SignUpService = () => {
+    return (
+
+);
+};
+
+export default SignUpService;
