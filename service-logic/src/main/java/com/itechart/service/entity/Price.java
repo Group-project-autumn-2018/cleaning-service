96af86ac1b8e1881e1d789f8b7a149272941502c
Vyacheslav <kleshchenok90@gmail.com>
2018-11-12 12:25:20 +0300

    companies

--- service-logic/src/main/java/com/itechart/service/entity/Price.java
+++ service-logic/src/main/java/com/itechart/service/entity/Price.java
@@ -1,26 +1,26 @@
-package com.itechart.service.entity;
-
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.stereotype.Component;
-
-import javax.persistence.*;
-import java.math.BigDecimal;
-
-@Getter
-@Setter
-@Entity
-@Component
-@Table(name = "prices")
-public class Price {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @ManyToOne
-    @JoinColumn(name = "company_id", referencedColumnName = "id", nullable = false)
-    private CleaningCompany company;
-    @Column(name = "base_price")
-    private BigDecimal basePrice;
-    @Column(name = "coefficient")
-    private BigDecimal coefficient;
-}
+//package com.itechart.service.entity;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//import org.springframework.stereotype.Component;
+//
+//import javax.persistence.*;
+//import java.math.BigDecimal;
+//
+//@Getter
+//@Setter
+//@Entity
+//@Component
+//@Table(name = "prices")
+//public class Price {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    @ManyToOne
+//    @JoinColumn(name = "company_id", referencedColumnName = "id", nullable = false)
+//    private CleaningCompany company;
+//    @Column(name = "base_price")
+//    private BigDecimal basePrice;
+//    @Column(name = "coefficient")
+//    private BigDecimal coefficient;
+//}
