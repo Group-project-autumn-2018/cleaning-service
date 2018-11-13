651fc48a1520ac669a444e0ea06098effd1aa5f5
Student <ira.corjowa2013@yandex.by>
2018-11-13 10:40:08 +0300

    add TimingOptionsDto

--- service-logic/src/main/java/com/itechart/service/util/CalculationRules.java
+++ service-logic/src/main/java/com/itechart/service/util/CalculationRules.java
@@ -7,59 +7,43 @@ import java.math.BigDecimal;
 
 public class CalculationRules {
 
-    public static BigDecimal PriceCalculation(PricingOptionsDto pricingOptionsDto){
-        BigDecimal price=BigDecimal.ZERO;
+    public static BigDecimal PriceCalculation(PricingOptionsDto pricingOptionsDto) {
+        BigDecimal price = BigDecimal.ZERO;
 
-            if(pricingOptionsDto==null){
-                throw new NullPointerException("return pricingOptionsDto is null at method PriceCalculation");
-            }
-            if(pricingOptionsDto.getCostOfCleaningSmallRooms()==null){
-                throw new NullPointerException("return value is null at method PriceCalculation");
-            }
-            if(pricingOptionsDto.getCostOfCleaningBigRooms()==null){
-                throw new NullPointerException("return value is null at method PriceCalculation");
-            }
-            if(pricingOptionsDto.getCostOfCleaningBathrooms()==null){
-                throw new NullPointerException("return value is null at method PriceCalculation");
-            }
+        if (pricingOptionsDto == null) {
+            throw new NullPointerException("return pricingOptionsDto is null at method PriceCalculation");
+        }
+        if (pricingOptionsDto.getCostOfCleaningSmallRooms() == null) {
+            throw new NullPointerException("return value is null at method PriceCalculation");
+        }
+        if (pricingOptionsDto.getCostOfCleaningBigRooms() == null) {
+            throw new NullPointerException("return value is null at method PriceCalculation");
+        }
+        if (pricingOptionsDto.getCostOfCleaningBathrooms() == null) {
+            throw new NullPointerException("return value is null at method PriceCalculation");
+        }
 
-
-            CalculationRules object =new CalculationRules();
-            /*price=(object.calculateCost(pricingOptionsDto.getNumberOfSmallRooms(),pricingOptionsDto.getCostOfCleaningSmallRooms())).add(
-                    object.calculateCost(pricingOptionsDto.getNumberOfBigRooms(),pricingOptionsDto.getCostOfCleaningBigRooms()).add(
-                            object.calculateCost(pricingOptionsDto.getNumberOfBathrooms(),pricingOptionsDto.getCostOfCleaningBathrooms()).multiply((
-                                    new BigDecimal(pricingOptionsDto.getCoefficient())
-                                    )
-                            )
-                    )
-            );*/
-
-            price=(new BigDecimal(pricingOptionsDto.getCoefficient())).multiply(
-                  new BigDecimal(pricingOptionsDto.getNumberOfSmallRooms()).multiply(pricingOptionsDto.getCostOfCleaningSmallRooms()).add(
-                         new BigDecimal(pricingOptionsDto.getNumberOfBigRooms()).multiply(pricingOptionsDto.getCostOfCleaningBigRooms()).add(
+        price = (new BigDecimal(pricingOptionsDto.getCoefficient())).multiply(
+                new BigDecimal(pricingOptionsDto.getNumberOfSmallRooms()).multiply(pricingOptionsDto.getCostOfCleaningSmallRooms()).add(
+                        new BigDecimal(pricingOptionsDto.getNumberOfBigRooms()).multiply(pricingOptionsDto.getCostOfCleaningBigRooms()).add(
                                 new BigDecimal(pricingOptionsDto.getNumberOfBathrooms()).multiply(pricingOptionsDto.getCostOfCleaningBathrooms())
-                         )
-                  )
-            );
+                        )
+                )
+        );
 
         return price;
     }
 
-    public  BigDecimal calculateCost(int itemQuantity, BigDecimal itemPrice){
-        BigDecimal itemCost  = itemPrice.multiply(new BigDecimal(itemQuantity));
-        return  itemCost;
-    }
 
-    public static double CountingTime(TimingOptionsDto object){
-        if(object==null){
+    public static double CountingTime(TimingOptionsDto object) {
+        if (object == null) {
             throw new NullPointerException("return value is null at method PriceCalculation");
         }
 
-       return object.getCoefficient()*( object.getNumberOfSmallRooms()*object.getTimeOfCleaningSmallRooms()+
-                object.getNumberOfBigRooms()*object.getTimeOfCleaningBigRooms()+
-                object.getNumberOfBathrooms()*object.getTimeOfCleaningBathrooms());
+        return object.getCoefficient() * (object.getNumberOfSmallRooms() * object.getTimeOfCleaningSmallRooms() +
+                object.getNumberOfBigRooms() * object.getTimeOfCleaningBigRooms() +
+                object.getNumberOfBathrooms() * object.getTimeOfCleaningBathrooms());
     }
-
 
 
 }
