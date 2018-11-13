a64eecb9e5d9b1ed91cf48fe43fabc5b8a4f7d8e
Student <ira.corjowa2013@yandex.by>
2018-11-13 10:45:25 +0300

    add tests and calculation price and time

---
+++ service-logic/src/main/java/com/itechart/service/dto/TimingOptionsDto.java
@@ -0,0 +1,16 @@
+package com.itechart.service.dto;
+
+
+import lombok.Data;
+
+@Data
+public class TimingOptionsDto {
+    private int numberOfSmallRooms;
+    private int numberOfBigRooms;
+    private int numberOfBathrooms;
+    private int timeOfCleaningSmallRooms;
+    private int timeOfCleaningBigRooms;
+    private int timeOfCleaningBathrooms;
+    private double coefficient;
+
+}
