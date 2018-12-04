825d9b528455441d9f42eea517f390573df06ca6
Student <dmitriylipgart@gmail.com>
2018-12-04 00:47:09 -0800

    little fix

--- web/src/main/resources/static/src/components/companies/company-confirm-modal-form.js
+++ web/src/main/resources/static/src/components/companies/company-confirm-modal-form.js
@@ -47,75 +47,78 @@ class CompanyConfirmModalForm extends Component {
 
     render() {
         return (
-            <div className="form-group row">
-                <div className="col-sm-8">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="confirmOrderModalTitle">Please confirm your order</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="col-sm-8 ">
-                        <div className="row">
-                            <div className="col-4">Address</div>
-                            <div className="col-8">{this.state.order.address}</div>
+            <React.Fragment>
+                <div className="form-group row">
+                    <div className="col-sm-8">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="confirmOrderModalTitle">Please confirm your order</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
-                        <div className="row">
-                            <div className="col-4">Cleaning Type</div>
-                            <div className="col-8">{this.state.order.cleaningType}</div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4">Rooms</div>
-                            <div className="col-8">
-                                <ul>
-                                    <li>{`Small rooms ${this.state.order.smallRoomsCount}`}</li>
-                                    <li>{`Big rooms ${this.state.order.bigRoomsCount}`}</li>
-                                    <li>{`Bathrooms ${this.state.order.bathroomsCount}`}</li>
-                                </ul>
+                        <div className="col-sm-8 ">
+                            <div className="row">
+                                <div className="col-4">Address</div>
+                                <div className="col-8">{this.state.order.address}</div>
                             </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4">Cleaning Date</div>
-                            <div className="col-8">{this.state.order.cleaningDay}</div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4">Cleaning Start Time</div>
-                            <div className="col-8">{this.state.order.cleaningTime}</div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4">Frequency</div>
-                            <div className="col-8">{this.state.order.frequency}</div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4">Duration</div>
-                            <div className="col-8">{this.state.order.duration}</div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4">Customer</div>
-                            <div className="col-8">{this.state.order.customer}</div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4">Estimated Price</div>
-                            <div className="col-8">{this.state.order.estimatedPrice}</div>
-                        </div>
-                        <div className="row">
-                            <div className="col-4">Estimated Time</div>
-                            <div className="col-8">{this.state.order.estimatedTime}</div>
-                        </div>
+                            <div className="row">
+                                <div className="col-4">Cleaning Type</div>
+                                <div className="col-8">{this.state.order.cleaningType}</div>
+                            </div>
+                            <div className="row">
+                                <div className="col-4">Rooms</div>
+                                <div className="col-8">
+                                    <ul>
+                                        <li>{`Small rooms ${this.state.order.smallRoomsCount}`}</li>
+                                        <li>{`Big rooms ${this.state.order.bigRoomsCount}`}</li>
+                                        <li>{`Bathrooms ${this.state.order.bathroomsCount}`}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-4">Cleaning Date</div>
+                                <div className="col-8">{this.state.order.cleaningDay}</div>
+                            </div>
+                            <div className="row">
+                                <div className="col-4">Cleaning Start Time</div>
+                                <div className="col-8">{this.state.order.cleaningTime}</div>
+                            </div>
+                            <div className="row">
+                                <div className="col-4">Frequency</div>
+                                <div className="col-8">{this.state.order.frequency}</div>
+                            </div>
+                            <div className="row">
+                                <div className="col-4">Duration</div>
+                                <div className="col-8">{this.state.order.duration}</div>
+                            </div>
+                            <div className="row">
+                                <div className="col-4">Customer</div>
+                                <div className="col-8">{this.state.order.customer}</div>
+                            </div>
+                            <div className="row">
+                                <div className="col-4">Estimated Price</div>
+                                <div className="col-8">{this.state.order.estimatedPrice}</div>
+                            </div>
+                            <div className="row">
+                                <div className="col-4">Estimated Time</div>
+                                <div className="col-8">{this.state.order.estimatedTime}</div>
+                            </div>
 
-                    </div>
-                    <div className="modal-footer">
+                        </div>
+                        <div className="modal-footer">
 
 
-                        <button type="submit" className="btn btn-secondary btn btn-danger" data-dismiss="modal"
-                                onClick={this.onClose}>Refuse order
-                        </button>
-                        <button type="submit" className="btn btn-secondary btn btn-success" data-dismiss="modal"
-                                onClick={this.onConfirm}>Confirm order
-                        </button>
+                            <button type="submit" className="btn btn-secondary btn btn-danger" data-dismiss="modal"
+                                    onClick={this.onClose}>Refuse order
+                            </button>
+                            <button type="submit" className="btn btn-secondary btn btn-success" data-dismiss="modal"
+                                    onClick={this.onConfirm}>Confirm order
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </React.Fragment>
+
         )
     }
 
