f9ff56c8ba8c0672f04d3f2e0909eebe4acd73ec
Student <dmitriylipgart@gmail.com>
2018-12-05 04:04:03 -0800

    added email message to customer

--- web/src/main/resources/static/src/components/api/api-actions.js
+++ web/src/main/resources/static/src/components/api/api-actions.js
@@ -17,12 +17,19 @@ export const fetchEntities = (page, size, entityURN, token, userID, search) => {
 };
 
 export const fetchCompaniesPOST = (entity, entityURN, token) => {
-    let options = {
-        headers: {
+    let header = {
+        'Content-Type': 'application/json'
+    };
+
+    if (token) {
+        header = {
             'Authorization': `Bearer ${token}`,
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-        },
+        }
+    }
+    let options = {
+        headers: header,
         method: 'POST',
         body: JSON.stringify(entity)
 
